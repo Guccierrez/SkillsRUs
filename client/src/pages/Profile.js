77b1b88a9b19a426e016786580b2a3d2ff5d6ac3
smurtee <muratees@yahoo.com>
2022-11-10 13:05:05 -0600

    profile skill form

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,48 +1,52 @@
-import React from 'react'
-import {  Button,
-    Checkbox,
-    Form,
-    Input,
-    Radio,
-    Select,
-    TextArea, } from 'semantic-ui-react'
-    
+import React from "react";
+import {
+  Button,
+  Checkbox,
+  Form,
+  Input,
+  Radio,
+  Select,
+  TextArea,
+} from "semantic-ui-react";
 
 const Profile = () => {
-   return(
+  return (
     <div>
-    <h1>Profile</h1>
-    <Form>
+      <h1>Profile</h1>
+       <Form>
     <Form.Group widths='equal'>
-      <Form.Field
-        control={Input}
+      <Form.Input
+        fluid
+        id='form-subcomponent-shorthand-input-first-name'
         label='First name'
         placeholder='First name'
       />
-      <Form.Field
-        control={Input}
+      <Form.Input
+        fluid
+        id='form-subcomponent-shorthand-input-last-name'
         label='Last name'
         placeholder='Last name'
       />
-    
-</Form.Group>
-<Form.Group inline>
- 
- 
-</Form.Group>
-<Form.Field
-  control={TextArea}
-  label='About'
-  placeholder='Tell us more about you...'
-/>
-<Form.Field
-  control={Checkbox}
-  label='I agree to the Terms and Conditions'
-/>
-<Form.Field control={Button}>Submit</Form.Field>
-</Form>
-</div>
-   )
-  };
+    </Form.Group>
+  </Form>
+  <Form>
+    <Form.Field inline>
+      <label>Skill/Service</label>
+      <Input placeholder='Skill/Service' />
+    </Form.Field>
+  </Form>
+  <Form>
+    <Form.Field>
+      <label>Skill/Service Description</label>
+      <input />
+    </Form.Field>
+  </Form>
+  <Button inverted color='olive'>
+        Add it!
+      </Button>
   
-  export default Profile;
+    </div>
+  );
+};
+
+export default Profile;
