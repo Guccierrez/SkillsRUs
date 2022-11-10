8c492341e12f07e404ddaa47f6fa99eaff67b586
austin <posty65841@gmail.com>
2022-11-10 12:52:30 -0600

    pushing up

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -14,8 +14,8 @@ function Signup(props) {
       variables: {
         email: formState.email,
         password: formState.password,
-        firstName: formState.firstName,
-        lastName: formState.lastName,
+        Name: formState.name,
+        
       },
     });
     const token = mutationResponse.data.addUser.token;
@@ -37,25 +37,16 @@ function Signup(props) {
       <h2>Signup</h2>
       <form onSubmit={handleFormSubmit}>
         <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
+          <label htmlFor="firstName">Your Name:</label>
           <input
-            placeholder="First"
+            placeholder="Name"
             name="firstName"
             type="firstName"
             id="firstName"
             onChange={handleChange}
           />
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            onChange={handleChange}
-          />
-        </div>
+       
         <div className="flex-row space-between my-2">
           <label htmlFor="email">Email:</label>
           <input
